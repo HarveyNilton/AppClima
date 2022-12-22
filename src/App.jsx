268713bec44da02eed292e943adbb48f55248aac
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2022-12-22 15:56:57 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import SplashScreeam from './components/SplashScreeam'
+
+
+function App() {
+  const [clima, setClima] = useState({})
+  const [change, setChange] = useState(true)
+
+  const [load, setLoad] = useState(false)
+  
+
+  useEffect(()=>{
+    setLoad(true)
+    function success(pos) {
+      const crd = pos.coords;
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=183d97d3803189cf9338bbc54a4dd296`)
+      .then(resp => setClima(resp.data))
+      
+    }
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+      navigator.geolocation.getCurrentPosition(success, error);
+
+     
+       setLoad(false)
+  },[])
+
+ 
+
+  const degreeCentigrade = clima.main?.temp-273.15
+  const turnIntoFarenheit = (degreeCentigrade*9/5)+32
+  const degree = degreeCentigrade.toFixed(2)
+  const farenheit = turnIntoFarenheit.toFixed(2)
+
+  const changeTemp = ()=>{
+    setChange(!change)
+  }
+  
+  return (
+    <div className="App">
+
+      {load && <SplashScreeam/>}
+      <div className='card-clima'>
+        <h3 className="title">Wheather App</h3>
+        <h4 className="sub-title">{`${clima.sys?.country} - ${clima.name}`}</h4>
+        <div className="contatiner-information">
+          <img className="image" src={`http://openweathermap.org/img/wn/${clima.weather?.[0].icon}@2x.png`} alt="" />
+          <section className="description">
+            <h4 className="scattered">"Scattered clouds"</h4>
+            <figure className='wind-speed'>
+              <img className='imag-icon' src="./public/windspeed.svg" alt="" />
+              <h4 className='item'>Wind speed:</h4><p>{clima.wind?.speed} m/s</p></figure>
+            <figure className='wind-speed'>
+              <img className='imag-icon' src="./public/cloud.svg" alt="" />
+            <h4 className='item'>Clouds:</h4><p>{clima.clouds?.all} %</p></figure>
+            <figure className='wind-speed'>
+              <img className='imag-icon' src="./public/presion.svg" alt="" />
+              <h4 className='item'>Pressure: </h4><p>{clima.main?.pressure} mbar</p></figure>
+          </section>
+        </div>
+        <h2 className="temperature">{`${change ? degree : farenheit} ${change ? 'ºC' : 'ºF'}`}</h2>
+        <button className='button' onClick={changeTemp}> <img className='icon-button' src={change ? "./public/centigradeicon.svg":"./public/farenheiticon.svg"} alt="" />{change ? 'To degress ºF':'To degress ºC'}</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SplashScreeam.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import '../index.css'
+
+const SplashScreeam = () => {
+
+    return (
+        <div className='load'>
+            <figure className='loads'>
+                Llloolalalalaal
+            </figure>
+        </div>
+    );
+};
+
+export default SplashScreeam;
